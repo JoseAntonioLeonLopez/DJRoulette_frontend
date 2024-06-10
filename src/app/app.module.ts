f3396d25df3a3b9a7d5e3cb9a1f3c96ca0e34d0c
Jose Antonio Leon Lopez <joseantonioleonlopez206@gmail.com>
2024-06-10 13:17:20 +0200

    Estructura inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
