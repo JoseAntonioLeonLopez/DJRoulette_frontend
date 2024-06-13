59c18cedec2d747b277ed3574e2beff186ac31ce
José Antonio León López <joseantonioleonlopez206@gmail.com>
2024-06-13 11:29:37 +0200

    Carga de canciones mejorada

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SongService } from '../../service/song.service';
 import { Song } from '../../models/song.model';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
   songs: Song[] = [];
   loading: boolean = true;
-  isModalOpen: boolean = false; // Agregar esta propiedad para controlar el estado del modal
+  isModalOpen: boolean = false;
 
-  constructor(private songService: SongService) {}
+  constructor(
+    private songService: SongService,
+    private cdr: ChangeDetectorRef // Inyectar ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.loadSongs();
@@ -23,16 +27,19 @@ export class LandingComponent {
     if (savedSongs) {
       this.songs = JSON.parse(savedSongs);
       this.loading = false;
+      this.cdr.detectChanges(); // Forzar la detección de cambios
     } else {
       this.songService.getAll().subscribe(
         (songs: Song[]) => {
           this.songs = songs;
           sessionStorage.setItem('songs', JSON.stringify(songs));
           this.loading = false;
+          this.cdr.detectChanges(); // Forzar la detección de cambios
         },
         (error) => {
           console.error('Error loading songs', error);
           this.loading = false;
+          this.cdr.detectChanges(); // Forzar la detección de cambios
         }
       );
     }
